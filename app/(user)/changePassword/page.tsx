9431e75efc725271e2348b393dcf0eeb0932a4f0
Gal Golan <gulilo@users.noreply.github.com>
2026-03-08 12:27:26 +0200

    sending email

--- app/(user)/changePassword/page.tsx
+++ app/(user)/changePassword/page.tsx
@@ -1,9 +1,11 @@
 import ChangePasswordForm from "./ChangePasswordForm";
+import EmailTestForm from "./emailTestForm";
 
 export default function ChangePasswordPage() {
   return (
     <div className="flex flex-col items-center justify-center min-h-full w-full">
-      <ChangePasswordForm />
+      {/* <ChangePasswordForm /> */}
+      <EmailTestForm />     
     </div>
   );
 }
