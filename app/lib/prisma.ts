2a723aa9dc15ed9d6f0b0cc59abd918fcee2cc4f
Gal Golan <gulilo@users.noreply.github.com>
2026-04-21 15:54:04 +0300

    fixed login issue on azure DB

--- app/lib/prisma.ts
+++ app/lib/prisma.ts
@@ -1,13 +1,20 @@
 import { PrismaClient } from "@prisma/client";
 import { PrismaNeon } from '@prisma/adapter-neon'
+import { PrismaPg } from "@prisma/adapter-pg";
 
 declare global {
   var prisma: PrismaClient | undefined;
 }
 
-const adapter = new PrismaNeon({
-  connectionString: process.env.POSTGRES_URL,
-});
+const connectionString = process.env.POSTGRES_URL;
+
+if (!connectionString) {
+  throw new Error("POSTGRES_URL is not defined");
+}
+
+const adapter = connectionString.includes("neon.tech")
+  ? new PrismaNeon({ connectionString })
+  : new PrismaPg({ connectionString });
 
 export const prisma =
   globalThis.prisma ??
